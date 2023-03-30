3ea0e3674d8d3e876699322b547ffa053f33eaec
Josh McKinney <joshka@users.noreply.github.com>
2023-03-30 13:18:59 -0700

    refactor(ui): Replace draw funcs with widgets
    - Add TitleBar and StatusBar structs
    - Implement Widget trait for them
    - Replace custom draw functions
    - Update App's draw to use new widgets

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,20 @@
 use crossterm::event::{KeyCode, KeyModifiers};
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::{sync::Mutex, task::JoinHandle};
 
 use crate::{tui::Tui, view::View, Event};
 use ratatui::{
-    backend::Backend,
+    buffer::Buffer,
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::Paragraph,
-    Frame,
+    widgets::{Paragraph, Widget},
 };
 use tokio::{sync::mpsc, time::interval};
 
 pub struct App {
-    rx: mpsc::Receiver<Event>,
-    tx: mpsc::Sender<Event>,
+    event_receiver: mpsc::Receiver<Event>,
+    event_sender: mpsc::Sender<Event>,
     tui: Tui,
     view: Arc<Mutex<View>>,
     messages: Vec<String>,
@@ -25,11 +24,11 @@ pub struct App {
 impl App {
     /// Build a new app.
     pub fn build() -> crate::Result<Self> {
-        let (tx, rx) = mpsc::channel(100);
-        let tui = Tui::build(tx.clone())?;
+        let (event_sender, event_receiver) = mpsc::channel(100);
+        let tui = Tui::build(event_sender.clone())?;
         Ok(Self {
-            rx,
-            tx,
+            event_receiver,
+            event_sender,
             tui,
             view: Arc::new(Mutex::new(View::login())),
             messages: Vec::new(),
@@ -42,11 +41,11 @@ impl App {
     pub async fn run(&mut self) -> crate::Result<()> {
         self.tui.init().await?;
         self.start_tick_handler();
-        let tx = self.tx.clone();
+        let event_sender = self.event_sender.clone();
         let view = self.view.clone();
         let view_task = tokio::spawn(async move {
             let mut view = view.lock().await;
-            view.run(tx).await;
+            view.run(event_sender).await;
         });
         self.handle_events().await?;
         self.drain_events().await?;
@@ -57,12 +56,12 @@ impl App {
     /// Start a tick handler that sends a tick event every `TICK_DURATION`.
     /// This is used to update the UI.
     fn start_tick_handler(&self) {
-        let tx = self.tx.clone();
+        let event_sender = self.event_sender.clone();
         tokio::spawn(async move {
             let mut interval = interval(crate::TICK_DURATION);
             loop {
                 interval.tick().await;
-                if tx.send(Event::Tick).await.is_err() {
+                if event_sender.send(Event::Tick).await.is_err() {
                     break;
                 }
             }
@@ -72,15 +71,15 @@ impl App {
     /// Drain the event queue.
     /// This is used to ensure that all events are processed before exiting.
     async fn drain_events(&mut self) -> crate::Result<()> {
-        self.rx.close();
-        while (self.rx.recv().await).is_some() {}
+        self.event_receiver.close();
+        while (self.event_receiver.recv().await).is_some() {}
         Ok(())
     }
 
     /// Handle events.
     /// This is the main event loop.
     async fn handle_events(&mut self) -> crate::Result<()> {
-        while let Some(event) = self.rx.recv().await {
+        while let Some(event) = self.event_receiver.recv().await {
             match event {
                 Event::Tick => {
                     self.tick_count += 1;
@@ -91,58 +90,43 @@ impl App {
                 }
                 Event::LoggedIn(login_details) => {
                     self.messages.push("Logged in!".to_string());
-                    let home = View::home(login_details);
-                    self.change_view(home).await;
+                    self.change_view(View::home(login_details)).await;
                 }
                 Event::LoggedOut => {
                     self.messages.push("Logged out!".to_string());
-                    let login = View::login();
-                    self.change_view(login).await;
+                    self.change_view(View::login()).await;
                 }
                 Event::Key(key) => {
                     let mut view = self.view.lock().await;
-                    if let View::Home(ref mut home) = *view {
+                    if let View::Home(ref mut home_view) = *view {
                         match (key.modifiers, key.code) {
                             (KeyModifiers::NONE, KeyCode::Char('j')) => {
-                                home.scroll_down();
+                                home_view.scroll_down();
                             }
                             (KeyModifiers::NONE, KeyCode::Char('k')) => {
-                                home.scroll_up();
+                                home_view.scroll_up();
                             }
-                            // (KeyModifiers::NONE, 'g') => {
-                            //     home.scroll_to_top();
-                            // }
-                            // (KeyModifiers::NONE, 'G') => {
-                            //     home.scroll_to_bottom();
-                            // }
-                            // (KeyModifiers::NONE, 'r') => {
-                            //     home.refresh();
-                            // }
-                            // (KeyModifiers::NONE, 'n') => {
-                            //     home.new_post();
-                            // }
-                            // (KeyModifiers::NONE, 'q') => {
-                            //     home.logout();
-                            // }
                             _ => {}
                         }
                     }
                 }
-                Event::MastodonError(err) => self.messages.push(err.to_string()),
+                Event::MastodonError(err) => {
+                    self.messages.push(err.to_string());
+                }
             }
         }
         Ok(())
     }
 
-    async fn change_view(&mut self, view: View) {
+    async fn change_view(&mut self, view: View) -> JoinHandle<()> {
         let mut current_view = self.view.lock().await;
         *current_view = view;
-        let tx = self.tx.clone();
+        let event_sender = self.event_sender.clone();
         let view = self.view.clone();
         tokio::spawn(async move {
             let mut view = view.lock().await;
-            view.run(tx).await;
-        });
+            view.run(event_sender).await;
+        })
     }
 
     async fn draw(&mut self) -> crate::Result<()> {
@@ -153,32 +137,63 @@ impl App {
             let layout = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints([
-                    Constraint::Length(1), // title bar
-                    Constraint::Min(3),    // main view
-                    Constraint::Length(1), // status bar
+                    Constraint::Length(TitleBar::HEIGHT),
+                    Constraint::Min(0),
+                    Constraint::Length(StatusBar::HEIGHT),
                 ])
                 .split(size);
 
-            draw_title_bar(frame, layout[0], view_title);
+            frame.render_widget(TitleBar::new(view_title), layout[0]);
             view.draw(frame, layout[1]);
-            draw_status_bar(frame, layout[2], format!("Tick: {}", self.tick_count));
+            frame.render_widget(
+                StatusBar::new(format!("Tick: {}", self.tick_count)),
+                layout[2],
+            );
         })?;
         Ok(())
     }
 }
 
-fn draw_title_bar(frame: &mut Frame<impl Backend>, area: Rect, title: String) {
-    let text = Spans::from(vec![
-        Span::styled("Tooters", Style::default().add_modifier(Modifier::BOLD)),
-        Span::raw(" | "),
-        Span::styled(title, Style::default().fg(Color::Gray)),
-    ]);
-    let title_bar = Paragraph::new(text).style(Style::default().fg(Color::White).bg(Color::Blue));
-    frame.render_widget(title_bar, area);
+struct TitleBar {
+    title: String,
 }
 
-fn draw_status_bar(frame: &mut Frame<impl Backend>, area: Rect, text: String) {
-    let text = Spans::from(vec![Span::raw(text)]);
-    let status_bar = Paragraph::new(text).style(Style::default().fg(Color::White).bg(Color::Red));
-    frame.render_widget(status_bar, area);
+impl TitleBar {
+    const HEIGHT: u16 = 1;
+    const fn new(title: String) -> Self {
+        Self { title }
+    }
+}
+
+impl Widget for TitleBar {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let style = Style::default().fg(Color::White).bg(Color::Blue);
+        let bold = Style::default().add_modifier(Modifier::BOLD);
+        let gray = Style::default().fg(Color::Gray);
+        let text = Spans::from(vec![
+            Span::styled("Tooters", bold),
+            Span::raw(" | "),
+            Span::styled(self.title, gray),
+        ]);
+        Paragraph::new(text).style(style).render(area, buf);
+    }
+}
+
+struct StatusBar {
+    text: String,
+}
+
+impl StatusBar {
+    const HEIGHT: u16 = 1;
+    const fn new(text: String) -> Self {
+        Self { text }
+    }
+}
+
+impl Widget for StatusBar {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let style = Style::default().fg(Color::White).bg(Color::Blue);
+        let text = Span::raw(self.text);
+        Paragraph::new(text).style(style).render(area, buf);
+    }
 }
