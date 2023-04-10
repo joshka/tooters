4ad3f19ee9ec02e935312fc5ec23175b5b8ff559
Josh McKinney <joshka@users.noreply.github.com>
2023-04-09 20:56:29 -0700

    refactor: pull out logging setup in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use anyhow::Context;
-use std::panic;
-use tooters::{app, logging::LogCollector};
+use parking_lot::Mutex;
+use std::{panic, sync::Arc};
+use tooters::{
+    app,
+    logging::{LogCollector, LogMessage},
+};
 use tracing::{error, info, metadata::LevelFilter};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let logs = setup_logging()?;
+    panic::set_hook(Box::new(|info| {
+        error!("Panic: {:?}", info);
+    }));
+    app::run(logs).await?;
+    info!("Exiting");
+    Ok(())
+}
+
+fn setup_logging() -> anyhow::Result<Arc<Mutex<Vec<LogMessage>>>> {
     let file_appender = tracing_appender::rolling::hourly("./", "tooters.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
 
     let log_collector = LogCollector::default();
     let logs = log_collector.logs();
-
     let subscriber = Registry::default()
         .with(EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into()))
         .with(
@@ -24,10 +37,5 @@ async fn main() -> anyhow::Result<()> {
     tracing::subscriber::set_global_default(subscriber)
         .context("setting default subscriber failed")?;
 
-    panic::set_hook(Box::new(|info| {
-        error!("Panic: {:?}", info);
-    }));
-    app::run(logs.clone()).await?;
-    info!("Exiting");
-    Ok(())
+    Ok(logs)
 }
