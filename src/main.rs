3d40aee8fbbf22aa6fed7d8582d0b248593bdd34
Josh McKinney <joshka@users.noreply.github.com>
2023-04-12 02:43:54 -0700

    fix: make sure logs are always flushed

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ use toot_rs::{
     logging::{LogCollector, LogMessage},
 };
 use tracing::{error, info, metadata::LevelFilter};
+use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let logs = setup_logging()?;
+    // _guard is a WorkerGuard which ensures that buffered logs are flushed to
+    // their output in the case of abrupt terminations of a process.
+    let (logs, _guard) = setup_logging()?;
+    info!("Starting");
     panic::set_hook(Box::new(|info| {
         error!("Panic: {:?}", info);
     }));
@@ -21,9 +25,12 @@ async fn main() -> anyhow::Result<()> {
 
 /// Sets up logging to a file and a collector for the logs that can be used to
 /// display them in the UI.
-fn setup_logging() -> anyhow::Result<Arc<Mutex<Vec<LogMessage>>>> {
-    let file_appender = tracing_appender::rolling::hourly("./", "toot-rs.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+fn setup_logging() -> anyhow::Result<(Arc<Mutex<Vec<LogMessage>>>, WorkerGuard)> {
+    let log_folder = xdg::BaseDirectories::with_prefix("toot-rs")
+        .context("failed to get XDG base directories")?
+        .get_state_home();
+    let file_appender = tracing_appender::rolling::hourly(log_folder, "toot-rs.log");
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
 
     let log_collector = LogCollector::default();
     let logs = log_collector.logs();
@@ -39,5 +46,5 @@ fn setup_logging() -> anyhow::Result<Arc<Mutex<Vec<LogMessage>>>> {
     tracing::subscriber::set_global_default(subscriber)
         .context("setting default subscriber failed")?;
 
-    Ok(logs)
+    Ok((logs, guard))
 }
