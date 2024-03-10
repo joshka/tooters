9cd41ae55839d86eb89bc0e21966ea881ada466e
Josh McKinney <joshka@users.noreply.github.com>
2024-03-10 06:42:07 -0700

    Rename toot-rs back to tooters

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod app;
-mod authentication;
-mod config;
-mod event;
-mod home;
-pub mod logging;
-mod root;
-mod ui;
-mod widgets;
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
-use anyhow::Context;
-use parking_lot::Mutex;
-use std::{panic, sync::Arc};
-use tooters::logging::{LogCollector, LogMessage};
-use tracing::{error, metadata::LevelFilter};
-use tracing_subscriber::{fmt, prelude::*, EnvFilter, Registry};
+use indoc::eprintdoc;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let _logs = setup_logging()?;
-    panic::set_hook(Box::new(|info| {
-        error!("Panic: {:?}", info);
-    }));
-    println!("Renamed to toot-rs. Please install that instead `cargo install toot-rs`.");
-    Ok(())
-}
+fn main() {
+    eprintdoc! {"
+        toot-rs was renamed to tooters. To install run:
 
-/// Sets up logging to a file and a collector for the logs that can be used to
-/// display them in the UI.
-fn setup_logging() -> anyhow::Result<Arc<Mutex<Vec<LogMessage>>>> {
-    let file_appender = tracing_appender::rolling::hourly("./", "toot-rs.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-
-    let log_collector = LogCollector::default();
-    let logs = log_collector.logs();
-    let subscriber = Registry::default()
-        .with(EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into()))
-        .with(
-            fmt::layer()
-                .with_writer(non_blocking)
-                .with_timer(tracing_subscriber::fmt::time::uptime()),
-        )
-        .with(log_collector);
-
-    tracing::subscriber::set_global_default(subscriber)
-        .context("setting default subscriber failed")?;
-
-    Ok(logs)
+            cargo install tooters --locked
+        
+        For more information, see https:://crates.io/crates/tooters or https://github.com/joshka/tooters
+    "};
 }
